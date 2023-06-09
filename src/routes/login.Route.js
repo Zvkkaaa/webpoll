6f92c9273862fb15f0d094adf102431a3beebd31
Zvkkaaa <zvkkaaa@gmail.com>
2023-06-09 10:05:56 +0800

    hiiii

--- src/routes/login.Route.js
+++ src/routes/login.Route.js
@@ -2,7 +2,7 @@ const { Login } = require("../controllers/login.Controller");
 const { protect} = require("../middleware/protect")
 
 const router = require("express").Router();
-
+//const router =require
 router.route("/login").post(Login);
 
 module.exports = router;
