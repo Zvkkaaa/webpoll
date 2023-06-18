8018de60c9649f9bf73e8a76aae3eb1cf562e3e2
Undercroft0 <seekndestroy0501@gmail.com>
2023-06-18 14:10:22 +0800

    fix poll attendance

--- src/routes/poll_attendance.Route.js
+++ src/routes/poll_attendance.Route.js
@@ -5,5 +5,5 @@ const { protect } = require("../middleware/protect");
 const router = require("express").Router();
 
 router.route("/:id").get(protect, getPollAttendance);
-router.route("/id/createPollAttendance/:answerid").post(protect,createPollAttendance);
+router.route("/:id/createPollAttendance/:answerid").post(protect,createPollAttendance);
 module.exports = router;
