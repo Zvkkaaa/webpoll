76243012ce28b24ab197b10e97daec2906804c7a
Unux7 <unuuthesupp123@gmail.com>
2023-06-26 11:00:51 +0800

    upload works

--- src/routes/upload.Route.js
+++ src/routes/upload.Route.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const upload= require("../middleware/upload.Middleware")
 const { uploadProfile, displayImage } = require("../controllers/upload.Controller");
 const { protect } = require("../middleware/protect");
 
 
-router.route("/:id/uploadImage").post(protect,upload,uploadProfile);
+router.route("/uploadImage").post(protect,uploadProfile);
 router.route("/:id/displayImage").get(displayImage);
 module.exports = router;
