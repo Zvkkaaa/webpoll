b246cf29668317baf131cf091833de0941ec7196
Unux7 <unuuthesupp123@gmail.com>
2023-06-26 14:04:12 +0800

    route going in geronimooo

--- src/routes/upload.Route.js
+++ src/routes/upload.Route.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const { uploadProfile, displayImage } = require("../controllers/upload.Controller");
 const { protect } = require("../middleware/protect");
 
+const { deleteProfile, updateProfile } = require("../controllers/upload.Controller");
 
 router.route("/uploadImage").post(protect,uploadProfile);
 router.route("/displayImage/:userid").get(displayImage);
+router.route("/deleteImage").delete(protect,deleteProfile);
+router.route("/updateImage").put(protect,updateProfile);
 module.exports = router;
