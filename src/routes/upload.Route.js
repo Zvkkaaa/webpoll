ec11670fef1c5a4e687e5ffb6fc44faabc68c797
Undercroft0 <seekndestroy0501@gmail.com>
2023-06-27 11:39:09 +0800

    default pfp

--- src/routes/upload.Route.js
+++ src/routes/upload.Route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { uploadProfile, displayImage, registerUploadProfile} = require("../controllers/upload.Controller");
 const { protect } = require("../middleware/protect");
 const { route } = require("./users.Route");
+const {setDefaultProfilePicture } = require("../controllers/upload.Controller");
 
 const { deleteProfile, updateProfile } = require("../controllers/upload.Controller");
 
@@ -10,4 +11,5 @@ router.route("/registerUploadImage/:userid").post(registerUploadProfile);
 router.route("/displayImage/:userId").get(displayImage);
 router.route("/deleteImage").delete(protect,deleteProfile);
 router.route("/updateImage").put(protect,updateProfile);
+router.route("/setDefaultProfilePicture/:userid").post(setDefaultProfilePicture);
 module.exports = router;
