7145d1684f1b16a6f1d40643a8f4eb9e4ee1a4c5
Unux7 <unuuthesupp123@gmail.com>
2023-07-19 16:52:23 +0800

    7-19 second

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,36 +1,39 @@
 //gonna use this github also ::: https://github.com/trulymittal/chat-socket.io
 
-
 const http = require("http");
 const socketIO = require("socket.io");
-let express = require("express");
-const app = express();
+const express = require("express");
+const cors = require('cors');
+const {writeAllChat, getAllChat} = require("../controllers/socket.Controller")
 
-const server = http.createServer();
-const io = socketIO(server);
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 let connectedUsers = 0;
 
 async function initialize() {
   console.log("Connecting to chatting server");
-
+  
   // Place your database initialization code here
   // For example, you can include the code for connecting to PostgreSQL using Sequelize
-  const socketRoute = require('../routes/socket.Route');
-  app.use("/allChat", socketRoute);
+
   io.on("connection", (socket) => {
     console.log("A user connected");
     connectedUsers++;
 
-    socket.on("chat message", (message) => {
-      console.log("Received message:", message);
-      io.emit("chat message", message);
-    });
+    socket.on("chat", (protect, message) => writeAllChat(protect, message));
 
     socket.on("disconnect", () => {
       console.log("A user disconnected");
       connectedUsers--;
     });
   });
+
   server.listen(process.env.SOCKET_PORT, () => {
     console.log(`Socket.IO server listening on port ${process.env.SOCKET_PORT}`);
   });
