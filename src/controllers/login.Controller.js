de12da55508c958d647a3e87461983c2cc3e5a9d
Undercroft0 <seekndestroy0501@gmail.com>
2023-07-19 11:23:00 +0800

    nice

--- src/controllers/login.Controller.js
+++ src/controllers/login.Controller.js
@@ -393,34 +393,38 @@ exports.changePassword= asyncHandler(async (req, res, next) => {
 
 });
 exports.getLoggedUser = asyncHandler(async (req, res, next) => {
-  console.log("ene bolj bn");
-  if (loggedUsers.length === 0) {
-    const users = await Users.findAll({
+  console.log("Fetching logged users...");
+
+  const users = await Users.findAll({
+    where: {
+      verified: true
+    }
+  });
+
+  const loggedUsers = [];
+
+  for (let i in users) {
+    console.log("Creating user list with offline logged user...");
+    const username = users[i].username;
+    const profilePic = await profile.findOne({
       where: {
-        verified: true
+        userid: users[i].id
       }
     });
-    console.log("ydj ene bolsoon");
-    for (let i in users) {
-      console.log("creating user list with offline logged");
-      const username = users[i].username;
-      const profilePic = await profile.findOne({
-        where: {
-          userid: users[i].id
-        }
-      });
-      const online = false;
-      const userList = {
-        username: username,
-        path: `/image/displayWithUsername/${username}`, // Endpoint to serve the image file directly
-        online: online,
-      }
-      loggedUsers.push(userList);
-    }
+    const online = false;
+    const userList = {
+      username: username,
+      path: `/image/displayWithUsername/${username}`, // Endpoint to serve the image file directly
+      online: online,
+    };
+    loggedUsers.push(userList);
   }
+
+  console.log("Fetched logged users:", loggedUsers);
 
   return res.status(200).json(loggedUsers);
 });
+
 exports.disconnect = asyncHandler(async (req, res, next) => {
   const disco = req.params.username;
   //gonna fix with update not delete
